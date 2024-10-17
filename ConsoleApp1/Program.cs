6d30ed698ce29edaa0445e79e49b61c5517b6c9d
AntonSkuratov <antonskuratov16@yandex.ru>
2024-10-17 03:27:30 +0300

    удалены ненужные проект

--- ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-using TodoListApp.Core.Entities;
-using TodoListApp.Storage;
-
-TodoListContext context = new TodoListContext();
-foreach(User user in context.Users.Include(u => u.DomainLogin).ToList())
-{
-	Console.WriteLine(user.Firstname + "  " + user.DomainLogin!.Login);
-}
